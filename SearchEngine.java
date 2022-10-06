c398d07bb011c05ac4d0bcdf64fbc82856bc6a4c
vtaylor <114200339+Von-Taylor@users.noreply.github.com>
2022-10-05 21:07:25 -0700

    Made SearchEngine

---
+++ SearchEngine.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> list = new ArrayList<>();
+    String listItems;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            listItems = "[";
+            
+            if(!list.isEmpty())
+                for(int i = 0; i < list.size(); i++) { listItems += list.get(i) + ", "; }
+            listItems += "]";
+
+            return String.format(listItems + "\nTry adding by doing \"/add?s=<something>\" or searching by doing /search?s=<something>");
+        } else if (url.getPath().equals("/add")) {
+            if(url.getQuery().isEmpty()) { return String.format("Input a valid query!"); }
+            
+            String[] parameters = url.getQuery().split("=");
+            list.add(parameters[1]);
+            
+            listItems = "[";
+            for(int i = 0; i < list.size(); i++) { listItems += list.get(i) + ", "; }
+            listItems += "]";
+            
+            return String.format(listItems + "\nAdded \"%s\" to the list", parameters[1]);
+        } else if(url.getPath().equals("/search")) {
+            if(url.getQuery().isEmpty()) { return String.format("Input a valid query!"); }
+
+            String[] parameters = url.getQuery().split("=");
+            
+            if(list.contains(parameters[1])) {
+                return String.format("\"%s\" is in the list! :D", parameters[1]);
+            } else {
+                return String.format("%s not found D:", parameters[1]);
+            }
+        } else {
+            System.out.println("Path: " + url.getPath());
+            String.format("\"" + url.getPath() + "\" Invalid");
+            
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
--- Server.java
+++ Server.java
@@ -48,6 +48,6 @@ public class Server {
 
         //start the server
         server.start();
-        System.out.println("Server Started! Visit http://localhost:" + port + " to visit.");
+        System.out.println("Server Started! Visit http://ieng6-203.ucsd.edu:" + port + " to visit.");
     }
 }
